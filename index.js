c5e9cbc11acfdf276fcc82268a816389dd91ad3b
Igorsml <igorsmlff@gmail.com>
2022-06-18 10:00:13 +0300

    feat: mobile, burger

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
+const burger = document.querySelector('.hamburger')
+const menu = document.querySelector('.nav-list')
+const links = document.querySelectorAll('.nav-link')
+const body = document.querySelector('body')
 
 
-[    ['header', 1, 1],
-    ['main', 1, 1],
-    ['footer', 1, 1],
-    ['section', 6, Infinity],
-    ['h1', 1, 1],
-    ['h2', 5, Infinity],
-    ['nav', 1, 1],
-    ['ul', 2, Infinity],
-    ['button', 10, Infinity],
-    ['input[type="email"]', 1, Infinity],
-    ['input[type="tel"]', 1, Infinity],
-    ['textarea', 1, Infinity],
-    ['input, textarea[placeholder]', 3, Infinity],
-]
-    .map((x) => [...x, [...document.querySelectorAll(x[0])].length])
-    .map((x) =>
-        x[3] >= x[1] && x[3] <= x[2]
-            ? `✔ ${x[0]} (${x[3]})`
-            : `✘ ${x[0]} (expected - ${x[1]}, actual - ${x[3]})`
-    )
-    .forEach((x) => console.log(x));
+burger.addEventListener('click', () => {
+  burger.classList.toggle('is-active')
+  menu.classList.toggle('active-list')
+  body.classList.toggle('fixed-position')
+})
 
-    console.log(`
+menu.addEventListener('click', (event) => {
+  if (event.target.classList.contains('nav-link')) {
+    menu.classList.remove('active-list')
+    burger.classList.remove('is-active')
+    body.classList.remove('fixed-position')
+  }
+})
+
+
+console.log(`
 - Вёрстка валидная (10/10)
 - Вёрстка семантическая (20/20)
 - Вёрстка соответствует макету (48/48)
